0838e8d7a604ef51e48da19a0a49a17950d2fdb8
aka-dekrain <48714286+aka-dekrain@users.noreply.github.com>
2020-03-06 14:00:30 +0600

    fix

--- arch1.sh
+++ arch1.sh
@@ -75,4 +75,4 @@ pacstrap /mnt base base-devel nano dhcpcd netctl
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/aka-dekrain/arch2018/master/arch2.sh)"
